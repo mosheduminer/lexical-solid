8bdaa934cbbf60f7b7665f12f414eccfe2994244
Moshe Uminer <mosheduminer@gmail.com>
2023-01-11 22:09:43 -0500

    update code for /shared folder

--- lexical-solid/src/shared/usePlainTextSetup.tsx
+++ lexical-solid/src/shared/usePlainTextSetup.tsx
@@ -5,16 +5,11 @@ import plainText from "@lexical/plain-text";
 import { mergeRegister } from "@lexical/utils";
 import { isServer } from "solid-js/web";
 
-import { InitialEditorStateType } from "./PlainRichTextUtils";
-
-export function usePlainTextSetup(
-  editor: LexicalEditor,
-  initialEditorState?: InitialEditorStateType
-) {
+export function usePlainTextSetup(editor: LexicalEditor) {
   if (!isServer) {
     onCleanup(
       mergeRegister(
-        plainText.registerPlainText(editor, initialEditorState),
+        plainText.registerPlainText(editor),
         registerDragonSupport(editor)
       ) // We only do this for init
     );
