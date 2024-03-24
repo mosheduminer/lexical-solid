6f8f6009d1c269e0873805c876b2f0e17054dfe5
Moshe Uminer <mosheduminer@gmail.com>
2024-03-24 13:57:10 -0400

    fix default import issue

--- lexical-solid/src/shared/usePlainTextSetup.tsx
+++ lexical-solid/src/shared/usePlainTextSetup.tsx
@@ -1,17 +1,14 @@
 import { onCleanup } from "solid-js";
 import { LexicalEditor } from "lexical";
 import { registerDragonSupport } from "@lexical/dragon";
-import plainText from "@lexical/plain-text";
+import { registerPlainText } from "@lexical/plain-text";
 import { mergeRegister } from "@lexical/utils";
 import { isServer } from "solid-js/web";
 
 export function usePlainTextSetup(editor: LexicalEditor) {
   if (!isServer) {
     onCleanup(
-      mergeRegister(
-        plainText.registerPlainText(editor),
-        registerDragonSupport(editor)
-      ) // We only do this for init
+      mergeRegister(registerPlainText(editor), registerDragonSupport(editor)) // We only do this for init
     );
   }
 }
